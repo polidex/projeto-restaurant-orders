83266dd89bd806fd1bcbede39d84c3c0c435334b
polidex <lp.igor@gmail.com>
2023-04-20 17:33:54 -0300

    tests: reqt 1

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,18 @@
 from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+# from tests.mocks import ingredients
 
 
-# Req 1
 def test_ingredient():
-    pass
+    salt = Ingredient('sal')
+    salt_2 = Ingredient('sal')
+
+    assert salt.__repr__() == "Ingredient('sal')"
+    assert salt.__eq__(salt_2)
+    assert salt.__hash__() == salt_2.__hash__()
+    assert salt.name == 'sal'
+    assert len(salt.restrictions) == 0
+
+
+    water = Ingredient('água')
+
+    assert salt.__hash__() != water.__hash__()
