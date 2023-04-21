369def9a34d13ab9d363fad9f3fb02f483ceb2df
polidex <lp.igor@gmail.com>
2023-04-21 16:18:46 -0300

    tests: reqt 2

--- tests/dish/test_dish.py
+++ tests/dish/test_dish.py
@@ -1,6 +1,26 @@
 from src.models.dish import Dish  # noqa: F401, E261, E501
+import pytest
+from models.ingredient import Ingredient
 
 
-# Req 2
 def test_dish():
-    pass
+    baiana = Dish('Fogazza Baiana', 12.00)
+    baiana_too = Dish('Fogazza Baiana', 12.00)
+
+    assert baiana.name == 'Fogazza Baiana'
+    assert baiana == baiana_too
+    assert hash(baiana) == hash(baiana_too)
+
+    atum = Dish('Fogazza Atum', 14.00)
+
+    assert hash(baiana) != hash(atum)
+    assert baiana.__repr__() == "Dish('Fogazza Baiana', R$12.00)"
+
+    baiana.add_ingredient_dependency(Ingredient('Azeitona'), 1)
+    assert Ingredient('Azeitona') in baiana.get_ingredients()
+    assert len(baiana.get_restrictions()) == 0
+
+    with pytest.raises(TypeError):
+        Dish('Fogazza Baiana', '12.00')
+    with pytest.raises(ValueError):
+        Dish('Fogazza Baiana', -12.00)
